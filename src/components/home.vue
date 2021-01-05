0181f9a055092fa1459fb4c030aec4db3cb26ea2
潘飞 <28442538@qq.com>
2021-01-05 19:45:46 +0800

    'login'

---
+++ src/components/home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bigcontent">
+    <el-button type="info" @click="logout"> 退出 </el-button>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.bigcontent{
+ height: 100%;
+}
+.el-container{
+  height: 100%;
+  .el-header{
+    background: #373D41;
+  }
+  .el-aside{
+    background: #373D41;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '../src/assets/global.css'
+import axios from 'axios'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
